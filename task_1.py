b4f6d69d5ba63b5067d6d21397d0282dd2b99905
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-01-21 17:18:54 +0600

    Задание выполнено

--- task_1.py
+++ task_1.py
@@ -1,1 +1,23 @@
-print("hello world")
+def plus_two(number):
+    """
+    прибавляет к заданному числу +2
+
+    Args:
+        number (int): заданное число
+
+    Returns:
+        int: результат
+    """
+    try:
+        result = number + 2
+        return result
+    except TypeError:
+        print("Ожидаемый тип данных — число!")
+
+
+if __name__ == "__main__":
+    try:
+        number = int(input("Введите число: "))
+        print(plus_two(number))
+    except ValueError:
+        print("Ожидаемый тип данных — число!")
